08585c307161c8f284a9f3eab7142ae5e84e3930
Sravani Sandhya Devi Pyla <sandhyapyla.8@gmail.com>
2023-07-04 09:54:21 +0530

    change the database

--- Marnstack/backendExpress/src/db/conn.js
+++ Marnstack/backendExpress/src/db/conn.js
@@ -1,11 +1,13 @@
-const mongoose =require ("mongoose")
-mongoose.set("strictQuery",false)
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
-
-mongoose.connect("mongodb+srv://EcommerceWeb:EcommerceWeb123@cluster0.rqasgf4.mongodb.net/Ecommerce")
-.then((e)=>{
-    console.log('connecetd')
-}).catch((e)=>{
-    console.log("not connecrted")
-})
-
+mongoose
+  .connect(
+    "mongodb+srv://fooddelivery:fooddelivery123@cluster0.x1grtaa.mongodb.net/Ecommerece"
+  )
+  .then((e) => {
+    console.log("connecetd");
+  })
+  .catch((e) => {
+    console.log("not connecrted");
+  });

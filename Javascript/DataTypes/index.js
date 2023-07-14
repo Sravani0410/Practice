5eca890d86cda0f9d281d7a751eb5424e73ccf3b
Sravani Sandhya Devi Pyla <sandhyapyla.8@gmail.com>
2023-07-14 11:11:29 +0530

    datatypes

---
+++ Javascript/DataTypes/index.js
@@ -0,0 +1,13 @@
+// Primitive DataType:
+// typeof "John Doe" // Returns "string"
+// typeof 3.14 // Returns "number"
+// typeof true // Returns "boolean"
+// typeof 234567890123456789012345678901234567890n // Returns bigint
+// typeof undefined // Returns "undefined"
+// typeof null // Returns "object" (kind of a bug in JavaScript)
+// typeof Symbol('symbol') // Returns Symbol
+
+
+// BigInt ---> after end of the value we are adding n
+// const alsoHuge = BigInt(9007199254740991);
+// console.log(alsoHuge)

20fedb4c5f11d9f95eb696e4a97ea8e951277b2d
Sravani Sandhya Devi Pyla <sandhyapyla.8@gmail.com>
2023-07-14 11:13:01 +0530

    add the javascript questions

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,11 +1,11 @@
 // 1.
-function sayHi() {
-  console.log(name);
-  console.log(age);
-  var name = "Lydia";
-  let age = 21;
-}
-sayHi();
+// function sayHi() {
+//   console.log(name);
+//   console.log(age);
+//   var name = "Lydia";
+//   let age = 21;
+// }
+// sayHi();
 // output: name:undefined ,age: reference Error
 // Explanation : Within the function, we first declare the name variable with the var keyword. This means that the variable gets hoisted (memory
 //     space is set up during the creation phase) with the default value of undefined, until we actually get to the line where we define the
@@ -16,12 +16,12 @@ sayHi();
 //     JavaScript throws a ReferenceError.
 
 // 2. What's the output?
-for (var i = 0; i < 3; i++) {
-  setTimeout(() => console.log(i), 1);
-}
-for (let i = 0; i < 3; i++) {
-  setTimeout(() => console.log(i), 1);
-}
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(() => console.log(i), 1);
+// }
+// for (let i = 0; i < 3; i++) {
+//   setTimeout(() => console.log(i), 1);
+// }
 // A: 0 1 2 and 0 1 2
 // B: 0 1 2 and 3 3 3
 // C: 3 3 3 and 0 1 2

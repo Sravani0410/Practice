9c10e2a0d8b83b20e16824a7efd0885c854ceff9
Sravani Sandhya Devi Pyla <sandhyapyla.8@gmail.com>
2023-07-17 14:18:14 +0530

    change the header

--- crudmern/client/src/components/Headers/Headers.js
+++ crudmern/client/src/components/Headers/Headers.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 const Headers = () => {
   return (
-    <div>Headers</div>
-  )
-}
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="">Home</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
 
-export default Headers
+export default Headers;
--- crudmern/client/src/pages/Register/Register.js
+++ crudmern/client/src/pages/Register/Register.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
 import "./Register.css";
 const Register = () => {
   return (
     <div className="container">
-      <h2 className="text-center">Register Your Details</h2>
+      <h2 className="text-center mt-1">Register Your Details</h2>
+      <Card className="shadow mt-3 p-3">
+        <div className="profile_div text-center">
+          <img src="../../man.jpg" />
+        </div>
+      </Card>
     </div>
   );
 };

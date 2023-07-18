481be35ef5d08da7e45155ca0c89fbc846ff4f72
Sravani Sandhya Devi Pyla <sandhyapyla.8@gmail.com>
2023-07-18 10:16:32 +0530

    change the helloworld vue folder

--- vue-project/src/components/HelloWorld.vue
+++ vue-project/src/components/HelloWorld.vue
@@ -7,36 +7,36 @@
   </div>
 </template>
 
-<!-- 1. composition Api ==> we have to pass the setup and this this is called modern API
 <script setup>
+//  1. composition Api ==> we have to pass the setup and this this is called modern API
 
 // Read props being passed to this component
-// import {ref} from "vue"
-// defineProps(["msg", "anotherMsg"]);
-// let count=ref(0);
-// const handleIncrement=()=>{
-//   count.value++
-//   console.log("ggfsgf",count.value)
-// }
-
-</script> -->
-<!--2. options API ===>old method some companies are still using this method -->
-<script>
-export default{
-  // reactive state data
-  data(){
-    return{
-      count:0,
-    }
-  },
-  // methods
-  methods:{
-    handleIncrement(){
-      this.count++
-    }
-  }
+import {ref} from "vue"
+defineProps(["msg", "anotherMsg"]);
+let count=ref(0);
+const handleIncrement=()=>{
+  count.value++
+  console.log("ggfsgf",count.value)
 }
 
 </script>
+<!--2. options API ===>old method some companies are still using this method 
+// <script></script>
+// export default{
+//   // reactive state data
+//   data(){
+//     return{
+//       count:0,
+//     }
+//   },
+//   // methods
+//   methods:{
+//     handleIncrement(){
+//       this.count++
+//     }
+//   }
+// }
+
+// </script>-->
 
 <style scoped></style>

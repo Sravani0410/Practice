9e1794c837a4cfde61ca3028daa079d0bc3298ee
himanshu <himanshugoyal@externlabs.com>
2023-07-31 11:54:22 +0530

    using react-paginate try to implement

--- crudmern/client/src/components/Pagination/Paginations.js
+++ crudmern/client/src/components/Pagination/Paginations.js
@@ -1,8 +1,33 @@
 import React from "react";
 import Pagination from "react-bootstrap/Pagination";
+import ReactPaginate from "react-paginate";
 const Paginations = ({ handleprev, handlenext, page, pagecount, setPage }) => {
+  console.log("handlenext========>",handlenext(),"handleprev=========>",handleprev(),"setPage========>",setPage,"page======>",page)
+  const handlePageClick = (data) => {
+    console.log("hjsgah====>",data.selected)
+    setPage(data.selected)
+  };
   return (
     <>
+      {/* <ReactPaginate
+        containerClassName="pagination justify-content-center"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pagecount}
+        breakClassName="page-item"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakLinkClassName="page-link"
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+        activeClassName="active"
+      /> */}
       {pagecount > 0 ? (
         <div className="pagination_div d-flex justify-content-end mx-5">
           <Pagination>
